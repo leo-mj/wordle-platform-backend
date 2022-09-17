7c7cd35eb8c78f8fb58ebcff58acee8710616811
Leo <leo.maedje@gmail.com>
2022-09-17 21:54:05 +0200

    Added solutions file

---
+++ utils/createTables.sql
@@ -0,0 +1,18 @@
+create table users (username varchar(30) not null, password varchar(30) not null, primary key (username));
+
+create table group_names (groupname varchar(30) not null, password varchar(30) not null, primary key (groupname));
+
+create table group_members (id serial primary key not null, groupname varchar(30) not null, username varchar(30) not null,
+                            foreign key (groupname) references group_names(groupname),
+                            foreign key (username) references users(username));
+
+create table results (result_id serial primary key not null, result_date varchar(30) not null, username varchar(30) not null, guess varchar(255) not null,
+                      foreign key (username) references users(username));
+                         
+                           
+
+
+
+
+
+
