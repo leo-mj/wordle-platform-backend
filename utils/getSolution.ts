4a9aeb1c77cbd4c039e93662ad1004a8904d17db
Leo <leo.maedje@gmail.com>
2022-10-19 15:45:19 +0100

    Reversed order of solution words

--- utils/getSolution.ts
+++ utils/getSolution.ts
@@ -5,7 +5,7 @@ export async function getSolution(date: string): Promise<string> {
   if (solution) {
     return solution;
   } else {
-    solution = (await client.query(`delete from word_list where word = (select word from word_list limit 1) returning word`)).rows[0];
+    solution = (await client.query(`delete from word_list where word = (select word from word_list order by id desc limit 1) returning word`)).rows[0];
     solution = (await client.query(`insert into target_words values ($1, $2) returning word`, [date, solution.word])).rows[0];
     return solution;
   }
